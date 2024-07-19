763d8e24d339252740a78ea8274a3fad6232421e
joviscab <152897880+joviscab@users.noreply.github.com>
2024-07-20 00:44:12 +0200

    add baisc files likedList.js and main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// example uses class syntax - adjust as necessary
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
